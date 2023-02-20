8b563881bf8f30d1576fda1cda982c1c4ce4924e
niravg96 <niravgirnari91@gmail.com>
2023-02-20 21:04:43 +0530

    SECOND COMMIT

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,167 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.ref.ReferenceQueue;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText email,pasword;
+    Button loginbutton;
+
+    String email_login = email.getText().toString().trim();
+    String password_login = pasword.getText().toString().trim();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email =findViewById(R.id.edittext_emailaddress_login);
+        pasword =findViewById(R.id.edittext_password_login);
+
+        loginbutton =findViewById(R.id.login_button);
+
+        String email_login = email.getText().toString().trim();
+        String password_login = pasword.getText().toString().trim();
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try
+                {
+                    if(email_login.isEmpty() || password_login.isEmpty())
+                    {
+                        Toast.makeText(LoginActivity.this, "Please Enter All Details", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        token();
+                    }
+                }
+                catch(Exception e)
+                {
+                    Toast.makeText(LoginActivity.this, "Error : "+e, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+    private void postlogindata(String access_token)
+    {
+        try
+        {
+            String url = "https://admin.p9bistro.com/index.php/SignIn";
+            RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                    email.setText("");
+                    pasword.setText("");
+                    try {
+                        JSONObject resobj = new JSONObject(response);
+                        String email = resobj.getString("email");
+                        String mobile_no = resobj.getString("mobile_no");
+                        Toast.makeText(LoginActivity.this, "Log In Successfully", Toast.LENGTH_SHORT).show();
+                        Log.e("Check", email + mobile_no);
+
+                    } catch (JSONException ex) {
+                        ex.printStackTrace();
+
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(LoginActivity.this, "Fail to get Response : "+error, Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Override
+                protected Map<String,String>getParams()
+                {
+                    Map<String,String> params = new HashMap<String,String>();
+                    params.put("email",email_login);
+                    params.put("password",password_login);
+                    params.put("login_by", String.valueOf(5));
+                    return params;
+                }
+                @Override
+                public Map<String,String>getHeaders()throws AuthFailureError
+                {
+                    Map<String,String>params = new HashMap<String ,String>();
+                    params.put("authorization",access_token);
+                    return params;
+                }
+            };
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(LoginActivity.this, "Error 3 : "+ex, Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void token()
+    {
+        String url = "https://admin.p9bistro.com/index.php/generate_auth_token";
+        Log.e("checklog", url + "");
+
+        StringRequest stringRequest =new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.e("checklog", response + "");
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(response);
+                    String access_token = jsonObject.getString("access_token");
+                    Log.e("ACCESSTOKEN", access_token);
+                    postlogindata(access_token);
+
+                } catch (JSONException je) {
+                    Toast.makeText(LoginActivity.this, "Error 2 : " + je, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.e("checklog",error + "");
+                Toast.makeText(getApplicationContext(), "Timeout Error", Toast.LENGTH_LONG).show();
+
+            }
+        }){
+                @Override
+                public Map<String,String> getHeaders() throws AuthFailureError{
+                    HashMap<String,String> headers = new HashMap<>();
+                    headers.put("x-api-key","XABRTYUX@123YTUFGB");
+                    return headers;
+                }
+
+        };
+        RequestQueue requestquese = Volley.newRequestQueue(getApplicationContext());
+        requestquese.add(stringRequest);
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,182 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+       EditText username2,password2,mobile_no2,email2;
+       Button register_btn;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) { 
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username2= findViewById(R.id.edittextusername);
+         password2= findViewById(R.id.edittextpassword);
+        mobile_no2= findViewById(R.id.edittextmobileno);
+        email2= findViewById(R.id.edittextemailaddress);
+        register_btn =findViewById(R.id.registerbutton);
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if(username2.getText().toString().isEmpty() || password2.getText().toString().isEmpty() || mobile_no2.getText().toString().isEmpty() || email2.getText().toString().isEmpty())
+                    {
+                        Toast.makeText(MainActivity.this, "Please Enter All Details", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        token();
+                     //   postDatausingVolley(username2.getText().toString().trim(),password2.getText().toString().trim(),mobile_no2.getText().toString().trim(),email2.getText().toString().trim());
+                    }
+                }
+                catch (Exception e)
+                {
+                    Toast.makeText(MainActivity.this, "Error "+e, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+       /* Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });*/
     }
+    private void postDatausingVolley(String access_token)
+    {
+        try
+        {
+            String url = "https://admin.p9bistro.com/index.php/SignUp";
+            RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+            StringRequest request = new StringRequest(Request.Method.POST, url, new com.android.volley.Response.Listener<String>() {
+
+                @Override
+                public void onResponse(String response) {
+                    username2.setText("");
+                    password2.setText("");
+                    email2.setText("");
+                    mobile_no2.setText("");
+
+                    try {
+                        JSONObject resobj = new JSONObject(response);
+                        String username = resobj.getString("username");
+                        String password = resobj.getString("password");
+                        String mobile_no = resobj.getString("mobile_no");
+                        String email = resobj.getString("email");
+
+                        Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                        Log.e("Check",username+password+mobile_no+email);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                    Toast.makeText(MainActivity.this, "Fail to get Response : "+error, Toast.LENGTH_SHORT).show();
+
+                }
+            }){
+                @Override
+                protected Map<String ,String> getParams()
+                {
+                    Map<String ,String> params = new HashMap<String,String>();
+                    params.put("username", String.valueOf(username2));
+                    params.put("password", String.valueOf(password2));
+                    params.put("mobile_no", String.valueOf(mobile_no2));
+                    params.put("email", String.valueOf(email2));
+                    params.put("profile","Nirav");
+                    params.put("register_by", String.valueOf(5));
+
+                    return params;
+                }
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("authorization", access_token);
+                    return params;
+
+                }
+            };
+
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(MainActivity.this, "Error 2 "+e, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    private void token() {
+
+        String url = "https://admin.p9bistro.com/index.php/generate_auth_token";
+
+        Log.e("checklog", url + "");
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.e("checklog", response + "");
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(response);
+                    String access_token = jsonObject.getString("access_token");
+                    Log.e("ACCESSTOKEN", access_token);
+                    postDatausingVolley(access_token);
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                                Log.e("checklog", error + "");
+                                Toast.makeText(getApplicationContext(), "Timeout Error", Toast.LENGTH_LONG).show();
+                            }
+                        }) {
+                            @Override
+                            public Map<String, String> getHeaders() throws AuthFailureError {
+                                HashMap<String, String> headers = new HashMap<>();
+                                headers.put("x-api-key", "XABRTYUX@123YTUFGB");
+                                return headers;
+                            }
+                        };
+                        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                        requestQueue.add(stringRequest);
+
+                    }
 }
