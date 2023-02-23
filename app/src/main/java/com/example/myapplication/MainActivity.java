8e16329aac618786c2c013189c872a18d7031456
niravg96 <niravgirnari91@gmail.com>
2023-02-23 22:55:55 +0530

    FOURTH COMMIT

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,6 +16,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -84,45 +85,65 @@ public class MainActivity extends AppCompatActivity
     }
     private void postregisterdata(String access_token)
     {
-
-
+            Log.e("Error ","1");
             String url = "https://admin.p9bistro.com/index.php/SignUp";
 
             RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+            Log.e("Error ","2");
+            JSONObject req = new JSONObject();
 
-            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            try
+            {
+                req.put("username",username_edt.getText().toString());  // DATA OF field which we will enter while doing sign up
+                req.put("password",password_edt.getText().toString());
+                req.put("mobile_no",mobileno_edt.getText().toString());
+                req.put("email",email_edt.getText().toString());
+                req.put("profile","nirav");
+                req.put("register_by", 5);
+            }
+            catch(Exception ex)
+            {
+                Toast.makeText( MainActivity.this, "Exception : "+ex, Toast.LENGTH_SHORT).show();
+            }
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url,req, new Response.Listener<JSONObject>() {
+
                 @Override
-                public void onResponse(String response)
+                public void onResponse(JSONObject response)
                 {
+                    Log.e("Error : ","2.1");
 
-                    try
+                try
                     {
-                        JSONObject resobj = new JSONObject(response);
+                        Log.e("Error ","3");
 
-                        if(resobj.getString("status").equals("true"))
+
+                        if(response.getBoolean("status"))
                         {
-                            String message = resobj.getString("message");
+                            Log.e("Error ","4");
+                            String message = response.getString("message");
                             Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
-
-                            JSONObject jsonData = resobj.getJSONObject("data");  // responses which we got after successful tun
-
+                            Log.e("Error ","5");
+                            JSONObject jsonData = response.getJSONObject("data");  // responses which we got after successful run
+                            Log.e("Error ","6");
                             String id = jsonData.getString("id");
                             String username = jsonData.getString("username");
                             String mobile_no = jsonData.getString("mobile_no");
                             String email = jsonData.getString("email");
                             String profile = jsonData.getString("profile");
                             String api_key = jsonData.getString("api_key");
+                            Log.e("Error ","7");
+                            Log.e("Data",id+username+mobile_no+email+profile+api_key);
 
                         }
-
                         username_edt.setText("");
                         password_edt.setText("");
                         mobileno_edt.setText("");
                         email_edt.setText("");
-
+                        Log.e("Error ","8");
                         Log.e("Check Data", username+password+email+mobileno);
 
-                    } catch (JSONException ex)
+                    } catch (Exception ex)
                     {
                         ex.printStackTrace();
                     }
@@ -134,24 +155,29 @@ public class MainActivity extends AppCompatActivity
                     Toast.makeText( MainActivity.this, "Fail to get Response : "+error, Toast.LENGTH_SHORT).show();
                 }
             }){
-                @Override
+              /*  @Override
                 protected Map<String,String> getParams()
                 {
                     Map<String,String> params = new HashMap<String,String>();
 
-                    params.put("username",username);  // DATA OF field which we will enter while doing sign up
-                    params.put("password",password);
-                    params.put("mobile_no",mobileno);
-                    params.put("email",email);
+                    params.put("username",username_edt.getText().toString());  // DATA OF field which we will enter while doing sign up
+                    params.put("password",password_edt.getText().toString());
+                    params.put("mobile_no",mobileno_edt.getText().toString());
+                    params.put("email",email_edt.getText().toString());
                     params.put("profile","nirav");
                     params.put("register_by", String.valueOf(5));
                     return params;
-                }
+
+                     //   Log.e("Data",username+password+mobileno);
+
+                }*/
                 @Override
                 public Map<String,String>getHeaders()throws AuthFailureError
                 {
                     Map<String,String>params = new HashMap<String ,String>();
+
                     params.put("authorization",access_token);
+                    params.put("Content-Type", "application/json");
                     return params;
                 }
             };
@@ -166,23 +192,28 @@ public class MainActivity extends AppCompatActivity
 
             Log.e("checklog", url + "");
 
-            StringRequest stringRequest =new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            StringRequest request =new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
 
                     Log.e("checklog", response + "");
                     JSONObject jsonObject = null;
                     try {
+
+                        Log.e("Error :","10");
                         jsonObject = new JSONObject(response);
+                        Log.e("Error :","11");
                         String access_token = jsonObject.getString("access_token");
+                        Log.e("Error :","12");
                         Log.e("ACCESSTOKEN", access_token);
                         postregisterdata(access_token);
+                        Log.e("Error :","13");
 
                     } catch (JSONException je) {
                         Toast.makeText(MainActivity.this, "Error 2 : " + je, Toast.LENGTH_SHORT).show();
                     }
                 }
-            }, new Response.ErrorListener() {
+            }, new Response.ErrorListener() {   // I AM GETTING ERROR HERE
                 @Override
                 public void onErrorResponse(VolleyError error) {
 
@@ -200,6 +231,6 @@ public class MainActivity extends AppCompatActivity
                 }
             };
             RequestQueue requestquese = Volley.newRequestQueue(getApplicationContext());
-            requestquese.add(stringRequest);
+            requestquese.add(request);
         }
     }
